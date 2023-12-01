9d1fdbce8f99358c57ffc07a6f75ab22afca1272
Mario Morcos <mariomorcoswassily@gmail.com>
2023-12-01 13:49:23 +0200

    added logic_token, added custom operator functions

--- PythonToVhdlConverter/tokens/__init__.py
+++ PythonToVhdlConverter/tokens/__init__.py
@@ -1,9 +1,9 @@
 import tdparser
-from logic_tokens import And_tok, Or_tok, Xor_tok
+from logic_tokens import And_tok, Or_tok, Xor_tok, Nand_tok, Nor_tok, Xnor_tok
 
 
 lexer = tdparser.Lexer(with_parens=True)
-lexer.register_token(And_tok, Or_tok, Xor_tok)
+lexer.register_token(And_tok, Or_tok, Xor_tok, Nand_tok, Nor_tok, Xnor_tok)
 
 def Parse(text):
     return lexer.parse(text)
